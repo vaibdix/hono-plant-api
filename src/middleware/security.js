64ef7a3fc7a10b65d6a102df333e22da4f2f925c
vaibdix <dixitvaib@gmail.com>
2024-12-27 18:56:52 +0530

    feat: add security headers

---
+++ src/middleware/security.js
@@ -0,0 +1,9 @@
+export const securityHeaders = async (c, next) => {
+  // Security headers
+  c.header('X-Content-Type-Options', 'nosniff')
+  c.header('X-Frame-Options', 'DENY')
+  c.header('X-XSS-Protection', '1; mode=block')
+  c.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
+  
+  await next()
+}
